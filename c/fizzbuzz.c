e1b249a92adb4869d9c8e0417c3731ee05c3b672
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2018-06-29 17:31:01 +0500

    improve fizzbuzz with bufSize

--- c/fizzbuzz.c
+++ c/fizzbuzz.c
@@ -2,17 +2,18 @@
 #include <string.h>
 
 int main(void) {
-	char s[10] = "";
+	int bufSize = 10;
+	char s[bufSize] = "";
 	for(int i = 1; i <= 100; i++) {
-		strncpy(s, "", 1);
+		strncpy(s, "", bufSize);
 		if (i % 3 == 0) {
-			strncat(s, "Fizz", 10);
+			strncat(s, "Fizz", bufSize);
 		}
 		if (i % 5 == 0) {
-			strncat(s, "Buzz", 10);
+			strncat(s, "Buzz", bufSize);
 		}
 		if (!strcmp(s, "")) {
-			snprintf(s, 10, "%d", i);
+			snprintf(s, bufSize, "%d", i);
 		}
 		if( ( (i - 1) % 10) == 0) {
 			printf("\n");
